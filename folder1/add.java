24ff710f6e90a7ca6f24cd7ba8acb5c98be39936
jatprerna <preranajat@gmail.com>
2021-06-12 11:24:43 +0530

    first commit

---
+++ folder1/add.java
@@ -0,0 +1,11 @@
+import java.util.*
+
+class Add{
+
+  public int add(int a,int b){
+  
+     return a+b;
+
+  }
+
+}
---
+++ folder1/difference.java
@@ -0,0 +1,11 @@
+import java.util.*
+
+class diff{
+
+  public int difference(int a,int b){
+  
+     return a-b;
+
+  }
+
+}
